a8925e08be820ad9e1c9232bb1c669a1246c150b
Rich-Merrett <162689463+Rich-Merrett@users.noreply.github.com>
2024-03-12 11:26:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ bp = Blueprint("routes", __name__, static_folder="static", template_folder="stat
 UI_TITLE = os.environ.get("UI_TITLE") or "Citation"
 UI_LOGO = os.environ.get("UI_LOGO")
 UI_CHAT_LOGO = os.environ.get("UI_CHAT_LOGO")
-UI_CHAT_TITLE = os.environ.get("UI_CHAT_TITLE") or "Citation Code Assist"
-UI_CHAT_DESCRIPTION = os.environ.get("UI_CHAT_DESCRIPTION") or "Get help with your code"
+UI_CHAT_TITLE = os.environ.get("UI_CHAT_TITLE") or "Citation Creative Content Assist"
+UI_CHAT_DESCRIPTION = os.environ.get("UI_CHAT_DESCRIPTION") or "Get help with creating your content"
 UI_FAVICON = os.environ.get("UI_FAVICON") or "/favicon.ico"
 UI_SHOW_SHARE_BUTTON = os.environ.get("UI_SHOW_SHARE_BUTTON", "true").lower() == "true"
 
