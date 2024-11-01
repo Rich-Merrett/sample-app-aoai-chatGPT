581fe4679df4dadfae2ff734d89cb3e8bfcece75
Rich-Merrett <162689463+Rich-Merrett@users.noreply.github.com>
2024-11-01 13:31:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ UI_TITLE = os.environ.get("UI_TITLE") or "iHasco"
 UI_LOGO = os.environ.get("UI_LOGO")
 UI_CHAT_LOGO = os.environ.get("UI_CHAT_LOGO")
 UI_CHAT_TITLE = os.environ.get("UI_CHAT_TITLE") or "iHasco Client Success Assist: Start chatting now"
-UI_CHAT_DESCRIPTION = os.environ.get("UI_CHAT_DESCRIPTION") or "Ask your help with your "
+UI_CHAT_DESCRIPTION = os.environ.get("UI_CHAT_DESCRIPTION") or "Ask your help with your client communications"
 UI_FAVICON = os.environ.get("UI_FAVICON") or "/favicon.ico"
 UI_SHOW_SHARE_BUTTON = os.environ.get("UI_SHOW_SHARE_BUTTON", "true").lower() == "true"
 
